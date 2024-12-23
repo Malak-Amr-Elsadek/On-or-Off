d3988bbb1500f28ca037a62e231ca926d8b1d3b9
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-23 16:16:18 +0300

    On or Off

---
+++ main.py
@@ -0,0 +1,8 @@
+def witch(on,off):
+    print("Do you want your device to be turned on or of ? Type (on/off)")
+
+ask=int(input("Enter on or of here please:"))
+if i in range(on):
+    print("Your device is on.")
+else:
+    print("Your device is off.")
